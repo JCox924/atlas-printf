479186ef5ed14a4024b85af648b456c35ff01042
JCox924 <Joshua.Cox@atlasschool.com>
2024-03-04 01:06:36 -0600

    removed Here: and goto here in favor of a for loop in _printf.c

--- _printf.c
+++ _printf.c
@@ -8,32 +8,37 @@ int _printf(const char *format, ...)
 
 	va_list args;
 	int i;
-	int t;
+	int t = 0;
 	int len;
+	int struc_size = sizeof(p) / sizeof(p[0]);
 
 	va_start(args, format);
 	
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
 		return (-1);
-Here:
+	}
+
 	while (format[i] != '\0')
 	{
-		t = 13;
-		while (t >= 0)
+		for (; t < struc_size; t++)
 		{
 			if(p[t].id[0] == format[i] && p[t].id[1] == format[i + 1])
 			{
-				len = len + p[t].f(args);
-				i = i + 2;
-				goto Here;
+				len += p[t].f(args);
+				i += 2;
+				break;
 			}
-			t--;
 
 		}
+		if (format[i] != '\0')
+		{
 		_putchar(format[i]);
 		i++;
 		len++;
+		}
 	}
+
 	va_end(args);
 	return (len);
 }
