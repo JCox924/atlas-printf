af1f970efc8c7f99459ffe65d3d45b3c1bb04095
DTBissy <isaactheedwards@gmail.com>
2024-03-04 04:29:08 +0000

    Were just doing everything we already have been its so simple

--- _printf.c
+++ _printf.c
@@ -2,3 +2,13 @@
 
 int _printf(const char *format, ...)
 {
+	printer p[] = 
+	{{"%s", printf_string}, {"%c", printf_char}, {"%%", print_percent}
+	};
+
+	va_list args;
+	int i;
+	int len;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'));
